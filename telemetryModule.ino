9fdda3b4afcb8626607d27745f3a702545684da5
JB-GB <jbgbtpg@outlook.es>
2024-06-21 17:58:28 -0600

    V.0.0.1 Not functional

--- telemetryModule.ino
+++ telemetryModule.ino
@@ -2,7 +2,10 @@
 
 /////////////////////////////
 /* Definicion de pines */
-
+int loraRX = 6; // Declarar el pin de Recibo de datos LORA
+int loraTX = 7; // Declarar el pin de Envio de datos LORA
+int gpsRX = 8; // Declarar el pin de Recibo de datos GPS
+int gpsTX = 9; // Declarar el pin de Envio de datos GPS
 
 /////////////////////////////
 
@@ -41,44 +44,56 @@ void setup() {  // Inicializacion del dispositivo
 
 void loop() { // Superbucle
   
-  // LLamar a getNMEA() para obtener posicion
+  /*
+    - Paquete de ejemplo  (Max packet size: 39 bytes)
+    RPM, tempSensor1, tempSensor2, tempSensor3, Latitud, Longitud
+    2500,149,178,165,25.2525252,31.31313113
+    
+    - Patron de eventos:
+      1. Recibir los datos con getRPM
 
+  */
+  
+  // Crea el paquete unto de datos a enviar
+  fPacket = String(getRPM()) + String(getTS1()) + String(getTS2()) + String(getTS3()) + getNMEA();
+
+  // Envia el paquete a funciones de envio
+  uploadLoRa(fPacket);
+  uploadMEM(fPacket);
+  
+
+  //Texto de ejemplo:
   if(Serial.available()){
     gpstest.println(Serial.read());
   }
   if(gpstest.available()){
     Serial.println(gpstest.read());
-  }
-
-  
-  /*  Paquete de ejemplo  */
-  // RPM, tempSensor1, tempSensor2, tempSensor3, Latitud, Longitud
-  // 2500,49,78,65,25.2525252,31.31313113
+  } 
 }
 
 int getRPM(){ // Funcion para extraer mediante <Metodo aun no definido> las revoluciones del vehiculo
 
-  return rpmVal;
+  return rpmVal; //Devolviendo variable con nombre generico
 }
 
 float getTS1(){ // Funcion para extraer el valor del sensor de temperatura 1
 
-  return ts1;
+  return ts1; //Devolviendo variable con nombre generico
 }
 
 float getTS2(){ // Funcion para extraer el valor del sensor de temperatura 2
 
-  return ts2;
+  return ts2; //Devolviendo variable con nombre generico
 }
 
 float getTS3(){ // Funcion para extraer el valor del sensor de temperatura 3
 
-  return ts3;
+  return ts3; //Devolviendo variable con nombre generico
 }
 
 String getNMEA(){ // Funcion para extraer los datos NMEA que necesitamos del GPS
 
-  return latlonGPS;
+  return latlonGPS; //Devolviendo variable con nombre generico
 }
 
 void uploadLoRa(String fPL){  // Funcion para publicar al modulo de radio a travez de Serial simulado el paquete final
@@ -86,7 +101,7 @@ void uploadLoRa(String fPL){  // Funcion para publicar al modulo de radio a trav
   // loraSerial.println(fPL);
 }
 
-void uploadMEM(String fPM){ // Funcion para publicar al modulo de almacenamiento localal
+void uploadMEM(String fPM){ // Funcion para publicar al modulo de almacenamiento local
   /*  
     Probablemente sea buena idea, manejar los datos en BSON/JSON para que los datos guardados sean exportables a la base de datos
   */
